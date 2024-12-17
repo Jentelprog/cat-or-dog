5d81c1567c292d620bdc29cdc11773e191e2eaa1
Jentelprog <ilyeschanchah@gmail.com>
2024-12-17 11:59:00 +0100

    adding the model and the python code to use it

---
+++ ai.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import fastai
+from fastai.learner import load_learner
+
+# Fix PosixPath issue
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# Load the learner
+learn = load_learner('export.pkl')
+
+from fastai.vision.all import *
+learn=load_learner('export.pkl')
+name='catdog'
+is_cat,_,probs = learn.predict(PILImage.create(name+'.png'))
+print(f" {name} is {is_cat}.")
+print(f"Probability that {name} is a cat: {probs[0]:.4f}")
+print(f"Probability that {name} is a dog: {probs[1]:.4f}")
